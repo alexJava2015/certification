235c9426c4126c8ef20e5b22a35f4443cd9997f9
Javier Alexander Bravo Rosales <jbravor@anzen.com.mx>
2016-08-07 21:53:08 -0500

    Add project to certification example

---
+++ src/mx/org/oracle/java/certification/chapterUno/ImportExample.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c)  2014 Anzen Soluciones S.A. de C.V.
+ * Mexico D.F.
+ * All rights reserved.
+ *
+ * THIS SOFTWARE IS  CONFIDENTIAL INFORMATION PROPIETARY OF ANZEN SOLUCIONES.
+ * THIS INFORMATION SHOULD NOT BE DISCLOSED AND MAY ONLY BE USED IN ACCORDANCE THE TERMS DETERMINED BY THE COMPANY ITSELF.
+ */
+package mx.org.oracle.java.certification.chapterUno;
+
+/**
+ * TODO [Add comments of the class] 
+ */
+public class ImportExample {
+
+	/**
+	 *  TODO [Add comments of method]
+	 *  @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/mx/org/oracle/java/certification/chapterUno/TestingChapter.java
@@ -0,0 +1,18 @@
+
+package mx.org.oracle.java.certification.chapterUno;
+
+/**
+ * TODO [Add comments of the class]
+ */
+ class Animal {
+	private String name;
+}
+
+class Animal298 {
+	
+}
+
+public  class TestingChapter {
+	
+	
+}
